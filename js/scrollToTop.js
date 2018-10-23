5cd851d2f90e53267ca2fa480bba184454a0e4a8
trionell <gustav.rakeberg@gmail.com>
2018-10-23 21:28:41 +0200

    Fixed scaling for mobile on template
    Fixed scaling issues for mobile in templete.html and other smaller stuffs. Also made the scroll to top button not cover footer when in "mobile scale"

--- js/scrollToTop.js
+++ js/scrollToTop.js
@@ -7,6 +7,25 @@ function scrollFunction() {
     } else {
         document.getElementById("scrollToTopBtn").style.display = "none";
     }
+    // Move scroll to top button away from footer on mobile
+    if(window.innerWidth < 992){
+        var footerHeight = document.getElementById("footerId").clientHeight;
+        var bodyHeight = Math.max(document.body.clientHeight, document.documentElement.clientHeight);
+        var windowSize = window.innerHeight;
+        var scrollPos = Math.max(document.body.scrollTop, document.documentElement.scrollTop);
+        var offset = 0;
+
+        if((scrollPos + windowSize) > (bodyHeight - footerHeight)){
+            offset = (scrollPos + windowSize) - (bodyHeight - footerHeight);
+            document.getElementById("scrollToTopBtn").style.bottom = (offset + 20).toString() + "px";
+        }
+        else{
+            document.getElementById("scrollToTopBtn").style.bottom = "20px";
+        }
+    }
+    else{
+        document.getElementById("scrollToTopBtn").style.bottom = "20px";
+    }
 }
 
 // When the user clicks on the button, scroll to the top of the document

6e105bb7161b95529c7232da192c12ff8b22fce1
felixbrunnegard <44345418+felixbrunnegard@users.noreply.github.com>
2018-11-10 11:42:08 +0100

    Leader with text

--- BranasCamp/database/migrations/2018_11_08_190108_registrations_leader.php
+++ BranasCamp/database/migrations/2018_11_08_190108_registrations_leader.php
@@ -13,7 +13,33 @@ class RegistrationsLeader extends Migration
      */
     public function up()
     {
-        //
+        Schema::create('registrations_leader', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->date('birthdate');
+            $table->string('last_four');
+            $table->string('address');
+            $table->string('zip');
+            $table->string('city');
+            $table->string('email');
+            $table->string('phonenumber');
+            $table->string('allergy');
+            $table->string('first_name_advocate');
+            $table->string('last_name_advocate');
+            $table->string('email_advocate');
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('phone_number_advocate');
+            $table->string('home_number');
+            $table->int('place');
+            $table->boolean('member');
+            $table->int('member_place');
+            $table->boolean('kitchen');
+            $table->tiemstamp('register_time');
+            $table->int('cost');
+            $table->string('other');
+            $table->boolean('terms');
+        });
     }
 
     /**
@@ -23,6 +49,6 @@ class RegistrationsLeader extends Migration
      */
     public function down()
     {
-        //
+        Schema::dropIfExists('tblregistrations_leader');
     }
 }

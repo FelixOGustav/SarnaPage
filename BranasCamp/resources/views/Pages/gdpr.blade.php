ac398289da7b3e92837c088cf859753ef7a985c1
felixbrunnegard <44345418+felixbrunnegard@users.noreply.github.com>
2018-11-11 22:28:27 +0100

    GDPR fix

--- BranasCamp/resources/views/Pages/gdpr.blade.php
+++ BranasCamp/resources/views/Pages/gdpr.blade.php
@@ -1,12 +1,21 @@
 @extends('Layouts/template')
 @section('content')
 
-<div class="centerTextInDiv" style="padding: 50px 5px;">
-    <h1>GDPR</h1>
-    <p>
-        Genom att anmäla dig och åka med på lägret godkänner du att vi får göra vad vi vill med dig och dina uppgifter.<br>
-        Samma gäller genom att använda denna sida.
-    </p>
+<div class="bg-white">
+    <div class="centerTextInDiv container" style="padding: 50px 5px;">
+        <h1 class="anmalan">GDPR</h1>
+        <h2>Allmänt</h2>
+        <h4> Vi på Branäslägret värnar om din personliga integritet. Därför jobbar vi ständigt med att ta hand om alla dina uppgifter på ett bra sätt som möjligt. Så att du kan känna dig trygg! Vi sparar endast uppgifter som vi har ett direkt syfte med och har laglig grund att spara. </h4>
+        <br>
+        <h2>Kontakt</h2>
+        <h4>Vid frågor angående vårat GDPR arbete eller utdrag av informtionen vi har på dig, kontakta: webb@branaslagret.se</h4>
+        <br>
+        <h2>Dina uppgifter</h2>
+        <h4>Branäslägret skickar inte dina personuppgifter till någon tredjepartsorganistaion. Däremot kommer uppgifter, så som personnummer, namn, adress, email(förälder), mobilnummer(förälder) skickas till Equmenia för att skapa en faktura.</h4>
+        <h4>Endast personer med behörighet kommer åt dina uppgifter i våran adminsida. Ingen användare (förutom lägerchefer, webb-admin och krisgruppsansvarig) kommer åt alla dina uppgifter. Användarna får endast se så mycket som deras säkerhetsnivå tillåter. Till exempel kommer matansvarig endast åt: namn, allergi och ort.</h4>
+        <h4>5 januari kommer alla förutom våra lägerchefer och webb-admin, bli av med alla rättigheter i våran adminsida. 1 maj 2019 kommer alla uppgifter i databasen raderas. Detta för att säkerhetsställa att dina uppgifter aldrig hamnar i fel händer.</h4> 
+        <h2>Bilder och film<h2>
+        <h4>Genom att anmäla dig till Branäslägret godkänner du att Branäslägret får publicera bilder och film från lägret på dig. Vill du dock inte vara med på bilder/film får du kontakta vår lägerledning via info@branaslagret.se och våra fotografer på lägret.</h4>
+    </div>
 </div>
-
 @endsection
